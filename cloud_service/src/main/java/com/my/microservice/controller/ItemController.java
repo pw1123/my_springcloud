db1fefddf62606d323044f091fac8f33d24f0b84
pw1123 <1076556139@qq.com>
2020-04-08 12:00:37 +0800

    服务接口,初始版本

---
+++ cloud_service/src/main/java/com/my/microservice/controller/ItemController.java
@@ -0,0 +1,31 @@
+package com.my.microservice.controller;
+
+import com.my.microservice.entity.Item;
+import com.my.microservice.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pengwei
+ * @version 1.0
+ * @date 2020/4/3 14:08
+ * @description
+ */
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    /**
+     * 对外提供接口服务，查询商品信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "item/{id}")
+    public Item queryItemById(@PathVariable("id") Long id) {
+        return this.itemService.queryItemById(id);
+    }
+}
