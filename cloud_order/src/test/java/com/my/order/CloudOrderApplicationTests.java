81cf4a3abf3a17dcb0c30360835f67a8dcc4baca
pw1123 <1076556139@qq.com>
2020-04-15 09:48:43 +0800

    打包方式的更改

--- cloud_order/src/test/java/com/my/order/CloudOrderApplicationTests.java
+++ cloud_order/src/test/java/com/my/order/CloudOrderApplicationTests.java
@@ -1,24 +1,14 @@
 package com.my.order;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 
 @SpringBootTest
 class CloudOrderApplicationTests {
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;//自动注入
-
     @Test
     void contextLoads() {
-        String serviceId = " cloud-service";
-        for (int i = 0; i < 20; i++) {
-            ServiceInstance serviceInstance = this.loadBalancerClient.choose(serviceId);
-            System.out.println("第" + (i + 1) + "次：" + serviceInstance.getHost() + ": " + serviceInstance.getPort());
-        }
+
     }
 
 }
